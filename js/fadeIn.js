75569fdc1894bd747b2d1bde661ec8b2061f3ce1
akirayzh <664753092@qq.com>
2019-11-28 01:06:51 +0800

    modified

--- js/fadeIn.js
+++ js/fadeIn.js
@@ -31,37 +31,52 @@ onload = function () {
 
     function setFadeIn(arr, direction) {
 
+        
+        document.body.style.height = document.body.offsetHeight;
+        document.body.style.width = document.body.offsetWidth;
         // document.body.style.overflow='hidden'
 
         for (i = 0; i < arr.length; i++) {
             // arr[i].style.position = 'relative';
             arr[i].style.opacity = 0;
-            arr[i].style.filter = 'alpha(opacity:0)'
-            
-            
+            arr[i].style.filter = 'alpha(opacity:0)';
             arr[i].style.transition = '0s';
 
             switch (direction) {
                 case 'left':
-                    arr[i].style.transform = 'translate(-100px,0)';
+                    arr[i].style.transform = 'translate(-50px,0)';
                     break;
                 case 'right':
-                    arr[i].style.transform = 'translate(100px,0)';
+                    arr[i].style.transform = 'translate(50px,0)';
                     break;
                 case 'top':
-                    arr[i].style.transform = 'translate(0,-100px)';
+                    arr[i].style.transform = 'translate(0,-50px)';
                     break;
                 case 'bottom':
-                    arr[i].style.transform = 'translate(0, 100px)';
+                    arr[i].style.transform = 'translate(0, 50px)';
                     break;
                 default:
                     break;
 
             }
+            // switch (direction) {
+            //     case 'left':
+            //         arr[i].style.margin = '0 0 0 -100px';
+            //         break;
+            //     case 'right':
+            //         arr[i].style.margin = '0 -100px 0 0';
+            //         break;
+            //     case 'top':
+            //         arr[i].style.margin = '-100px 0 0 0';
+            //         break;
+            //     case 'bottom':
+            //         arr[i].style.margin = '0 0 -100px 0';
+            //         break;
+            //     default:
+            //         break;
 
-            // setTimeout(function(){
-            //     arr[i].style.transition = '1s';
-            // }, 1);
+            // }
+            
             
 
 
@@ -74,16 +89,19 @@ onload = function () {
         
         var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
         var browserHeight = window.innerWidth || document.documentElement.clientHeight || document.body.clientHeight;
-        var triggerHeight = scrollTop + browserHeight/5;
+        var triggerHeight = scrollTop + browserHeight/2;
 
 
         console.log(scrollTop);
         for (i = 0; i < arr.length; i++) {
             if (triggerHeight > arr[i].offsetTop) {
+                // arr[i].style.display='block';
                 arr[i].style.transform = 'translate(0,0)';
+                // arr[i].style.margin = '0'
                 arr[i].style.transition = '1s'
                 arr[i].style.opacity = '1';
                 arr[i].style.filter = 'alpha(opacity:100)';
+                
             }
         }
         return false;
