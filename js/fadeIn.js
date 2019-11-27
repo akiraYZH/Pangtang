68238967a97ffee09c5fb4cbf333b1617b8f0c17
akirayzh <664753092@qq.com>
2019-11-27 20:31:58 +0800

    modified

--- js/fadeIn.js
+++ js/fadeIn.js
@@ -5,6 +5,7 @@ FadeIn();
 function FadeIn(){
 
     var aFadeIn = new Array();
+    // var 
     aFadeIn.push(document.querySelectorAll('.fadeInL'));
     aFadeIn.push(document.querySelectorAll('.fadeInR'));
     aFadeIn.push(document.querySelectorAll('.fadeInT'));
@@ -17,10 +18,11 @@ function FadeIn(){
             aFadeIn[i].style.opacity=0;
             aFadeIn[i].style.filter='alpha(opacity:0)'
             aFadeIn[i].style.transition = '1s';
+            alert(aFadeIn.direction)
             
         }
 
         // if()
-        aFadeIn[i].style.left= -200+'px';
+        // aFadeIn[i].style.left= -200+'px';
     
 } 
