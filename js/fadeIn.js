a8e53c79fcf5f2b8a44b68878ca3b89b8cd09907
akirayzh <664753092@qq.com>
2019-11-27 20:16:26 +0800

    project added

---
+++ js/fadeIn.js
@@ -0,0 +1,26 @@
+FadeIn();
+
+
+
+function FadeIn(){
+
+    var aFadeIn = new Array();
+    aFadeIn.push(document.querySelectorAll('.fadeInL'));
+    aFadeIn.push(document.querySelectorAll('.fadeInR'));
+    aFadeIn.push(document.querySelectorAll('.fadeInT'));
+    aFadeIn.push(document.querySelectorAll('.fadeInB'));
+    // alert(aFadeIn.length);
+    
+        for(i=0;i<aFadeIn.length;i++){
+            aFadeIn[i].style.position='relative';
+            
+            aFadeIn[i].style.opacity=0;
+            aFadeIn[i].style.filter='alpha(opacity:0)'
+            aFadeIn[i].style.transition = '1s';
+            
+        }
+
+        // if()
+        aFadeIn[i].style.left= -200+'px';
+    
+} 
